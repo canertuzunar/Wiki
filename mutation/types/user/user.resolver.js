c1421a8c076a532932b91515337cf20ee987bbb4
tuzunar <176001027@stu.adu.edu.tr>
2020-10-19 15:54:42 +0300

    UPDATE: add user query and update, remove, create resolver

--- mutation/types/user/user.resolver.js
+++ mutation/types/user/user.resolver.js
@@ -1,15 +1,39 @@
 import  UserModel  from "./user.model"
 import bcrypt from'bcrypt'
 const UserResolver = {
-    createUser: async (args) => {
-        const hashPass = await bcrypt.hash(args.input.password, 10)
-        const User = { 
-            username: args.input.username,
-            password: hashPass,
-            email: args.input.email
+    user: async (args) => {
+        const {username, password} = args
+        const user = await UserModel.findOne({
+            username: username
+        })
+        .lean()
+        .exec()
+
+        const valid = await bcrypt.compare(password, user.password)
+        if(!valid){
+            throw new Error('its not valid')
         }
+
+        return user
+    },
+    createUser: async (args) => {
+        const {username, password, email} = args.input
+        const hashPass = await bcrypt.hash(password, 10)
+        const User = {username, password: hashPass, email}
         return await UserModel.create(User)
+    },
+    removeUser: async (args) => {
+        return await UserModel.findOneAndRemove({name:args.name})
+        .lean()
+        .exec()
+    },
+    updateUser: async (args) => {
+        const {name, input} = args
+        return await UserModel.findOneAndUpdate(name, input, {new: true})
+        .lean()
+        .exec() 
     }
 }
+
 
 export default UserResolver
