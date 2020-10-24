8ef4ad8c460a1ce6df8115791bd60398613ffbc1
tuzunar <176001027@stu.adu.edu.tr>
2020-10-24 16:35:50 +0300

    ADD: router and home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route} from'react-router-dom'
+import { Home } from './pages';
+import './App.scss'
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+const App = () => {
+    return (
+      <Router>
+        <Switch>
+        <Route path="/">
+          <Home/>
+        </Route>
+        </Switch>
+      </Router>
+    )
 }
 
-export default App;
+export default App
+
