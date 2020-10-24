a4f2fd4e1edae19fa3e5f8efcd34f8c913d27750
tuzunar <176001027@stu.adu.edu.tr>
2020-10-24 19:25:16 +0300

    ADD: Easy Import

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,10 @@
+import Header from'./Header/Header'
+import Footer from'./Footer/Footer'
+import Layout from'./Layout/Layout'
+import Link from'./Link/Link'
+export {
+    Footer,
+    Header,
+    Layout,
+    Link
+}
