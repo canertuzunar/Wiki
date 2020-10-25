be39c51fc779ab8d2271645e539f19424996cad5
tuzunar <176001027@stu.adu.edu.tr>
2020-10-25 17:41:16 +0300

    ADD: Header components

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import styles from'./Header.module.scss'
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            Menu-Button GameWiki sign-in
+        </header>
+    )
+}
+
+
+export default Header
