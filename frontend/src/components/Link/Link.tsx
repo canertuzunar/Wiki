ef9f2e19ad92313c278d05ff33bf893b0a35f20c
tuzunar <176001027@stu.adu.edu.tr>
2020-10-25 17:42:32 +0300

    ADD: Link components

---
+++ frontend/src/components/Link/Link.tsx
@@ -0,0 +1,13 @@
+import React from'react'
+
+const Link = () => {
+    return (
+        <ul>
+            <li>choose language</li>
+            <li>feedback</li>
+            <li>social media</li>
+        </ul>
+    )
+}
+
+export default Link
