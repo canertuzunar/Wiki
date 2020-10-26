b6c7129f47ad410283e2e4c536265ac1fb548255
tuzunar <176001027@stu.adu.edu.tr>
2020-10-26 17:06:35 +0300

    EDIT: Sticky footer and flexbox

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -4,11 +4,9 @@ import styles from'./Footer.module.scss'
 const Footer = () => {
     return (
         <footer id={styles.footer}>
-            <ul className={styles.footerList}>
-                <li>choose language</li>
-                <li>social media</li>
-                <li>feedback</li>
-            </ul>
+            <p>choose language</p>
+            <p>feedback</p>
+            <p>social media</p>
         </footer>
     )
 }
