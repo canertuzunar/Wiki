0752a8cc38ee35d50ca9016a071e0453d551141f
tuzunar <176001027@stu.adu.edu.tr>
2020-10-26 17:04:02 +0300

    ADD: easy import

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,9 +2,11 @@ import Header from'./Header/Header'
 import Footer from'./Footer/Footer'
 import Layout from'./Layout/Layout'
 import Link from'./Link/Link'
+import Logo from'./Logo/Logo'
 export {
     Footer,
     Header,
     Layout,
-    Link
+    Link,
+    Logo
 }
