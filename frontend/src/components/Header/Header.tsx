d7d3db4de2e64291966e0ad323fb0aa07773232c
tuzunar <176001027@stu.adu.edu.tr>
2020-10-27 17:50:39 +0300

    EDIT: display type, ADD: Logo Component and css maps

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,9 +1,13 @@
 import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom'
+import { Logo } from '..'
 import styles from'./Header.module.scss'
 const Header = () => {
     return (
         <header className={styles.header}>
-            Menu-Button GameWiki sign-in
+            <p>Button</p>
+            <Logo/>
+            <p>sign in</p>
         </header>
     )
 }
