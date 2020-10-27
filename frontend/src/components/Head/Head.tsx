2c003f60edd2068aaa2e9e54f152c50fdc01f39c
tuzunar <176001027@stu.adu.edu.tr>
2020-10-27 17:46:45 +0300

    ADD: Head component

---
+++ frontend/src/components/Head/Head.tsx
@@ -0,0 +1,20 @@
+import React from'react'
+import Helmet from'react-helmet'
+
+interface HeadInterface {
+    title?: String,
+}
+
+const Head: React.FC<HeadInterface> = ({title}) => {
+    return(
+        <Helmet>
+            <title> GameWiki - {title}</title>
+        </Helmet>
+    )
+}
+
+Head.defaultProps ={
+    title: ' '
+}
+
+export default Head
