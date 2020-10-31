2e3895693a2ae7af75a1fe79f5e0b21ae5c6d127
tuzunar <176001027@stu.adu.edu.tr>
2020-10-31 19:58:48 +0300

    EDIT: replaced master scss instead app scss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from'react-router-dom'
 import { Home } from './pages';
-import './App.scss'
+import './util/master.scss'
 
 
 const App = () => {
