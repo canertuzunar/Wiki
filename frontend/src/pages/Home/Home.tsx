ee118b83ec8454a9cafaeca6d57d7403916782d7
tuzunar <176001027@stu.adu.edu.tr>
2020-11-02 19:08:36 +0300

    ADD: seach input, secondary text , responsive design v0.1

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,10 +1,22 @@
 import React from'react'
 import { Layout } from '../../components'
-
+import style from './Home.module.scss'
+import cn from'classnames'
+import xbox from '../../image/xbox.png'
 const Home = () => {
     return (
         <Layout>
-            Home
+            <div className={style.container}>
+                <div className={style.search}>
+                    <input type="text" name="" className={cn(style.searchBar, style.bold)} placeholder="Search GameWiki"/>
+                    <p className={cn(style.secondaryText, style.bold, style.font24, style.SearchText)}>
+                        Type what you are looking for...
+                    </p>
+                </div>
+                <div className={style.image}>
+                    <img src={xbox} alt=""/>
+                </div>
+            </div>
         </Layout>
     )
 }
