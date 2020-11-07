ac809a848c7c791583aaeb4800ca0d7c39ed63e6
tuzunar <176001027@stu.adu.edu.tr>
2020-11-08 01:48:00 +0300

    ADD: Content page and style

---
+++ frontend/src/pages/Content/Content.tsx
@@ -0,0 +1,14 @@
+import React from 'react' 
+import style from 'Content.module.scss'
+import { Layout } from '../../components'
+
+const Content = () => {
+    return(
+        <Layout>
+            <p>Content page is here</p>
+        </Layout>
+    )
+}
+
+
+export default Content
