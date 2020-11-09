37c90c7d84f7808bed8c6e0756f5d0b2b33a7c96
tuzunar <176001027@stu.adu.edu.tr>
2020-11-09 17:51:28 +0300

    ADD: Store provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,20 +2,22 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from'react-router-dom'
 import { Content, Home } from './pages';
 import './util/master.scss'
-
+import {StoreProvider} from'./util/store/store'
 
 const App = () => {
     return (
-      <Router>
-        <Switch>
-        <Route path="/content">
-          <Content/>
-        </Route>
-        <Route path="/">
-          <Home/>
-        </Route>
-        </Switch>
-      </Router>
+      <StoreProvider>
+        <Router>
+          <Switch>
+          <Route path="/content">
+            <Content/>
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+          </Switch>
+        </Router>
+      </StoreProvider>
     )
 }
 
