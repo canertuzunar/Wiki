5b279da5681aae22fc8c12d02e47a958b6d10d87
tuzunar <176001027@stu.adu.edu.tr>
2020-11-10 17:39:28 +0300

    ADD: i18next init, import i18n and localize resources

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import i18next from 'i18next';
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from'react-router-dom'
 import { Content, Home } from './pages';
 import './util/master.scss'
 import {StoreProvider} from'./util/store/store'
+import resources from'./util/localize/resources'
+
+i18next.init({
+  resources,
+  lng: "en"
+})
 
 const App = () => {
     return (
