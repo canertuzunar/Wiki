ede2414ad713f0db0e940144233f0a3e62a20af3
tuzunar <176001027@stu.adu.edu.tr>
2020-11-10 17:38:12 +0300

    ADD: translation resource

---
+++ frontend/src/util/localize/resources.ts
@@ -0,0 +1,19 @@
+import i18n from'i18next'
+
+const resources = {
+    en: {
+        translation: {
+            SearchInput: "Search GameWiki",
+            secondaryText: "Type what you are looking for..."
+        }
+    },
+    tr: {
+        translation: {
+            SearchInput: "GameWiki'de Ara",
+            secondaryText: "Aramak Istedigin Oyunu yaz..."
+        }
+    }
+}
+
+
+export default resources
