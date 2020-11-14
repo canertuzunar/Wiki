f883ebb20ccd374c6f0cd08aba8fa53ce79c68f0
tuzunar <176001027@stu.adu.edu.tr>
2020-11-14 17:36:27 +0300

    EDIT: scroll detection with synthetic event and change header style

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,17 +1,30 @@
 import React, { Fragment } from'react'
+import { useStore } from '../../util/store/store'
 import {Header, Footer, Head} from'../index'
+import styles from'./Layout.module.scss'
 
 interface LayoutProps {
     children: React.ReactNode
 }
 
 const Layout : React.FC<LayoutProps> = ({children}) => {
+    const {state, dispatch} = useStore()
+    const scrollDetector = (scrollY:number) => {
+    scrollY > 30 ? dispatch({type:"setShadow", value:true}) : dispatch({type:"setShadow", value:false})
+    }
     return (
         <Fragment>
-            <Head/>
-            <Header/>
+            <div className={styles.container} onScroll={(e) => {
+                e.persist()
+                const element = e.target as HTMLBodyElement
+                scrollDetector(element.scrollTop)
+
+            }}>
+                <Head/>
+                <Header />
                 {children}
-            <Footer/>
+                <Footer/>
+            </div>
         </Fragment>
     )
 }
