07c1632c86d6923db63951d236e1f244bb0d2b1a
tuzunar <176001027@stu.adu.edu.tr>
2020-11-16 22:06:01 +0300

    EDIT: added detectRoute function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 import { Switch, Route, useLocation} from'react-router-dom'
 import { Content, Home } from './pages';
 import './util/master.scss'
-import {StoreProvider, useStore} from'./util/store/store'
+import {useStore} from'./util/store/store'
 import resources from'./util/localize/resources'
 
 i18next.init({
@@ -13,22 +13,23 @@ i18next.init({
 
 const App = () => {
   const location = useLocation()
-  console.log(location)
-  useEffect(() => {
-    const{state, dispatch} = useStore()
+  const {state, dispatch} = useStore()
+  const detectRoute = () => {
     dispatch({type:"setShadow", value:false})
+    console.log(location, state)
+  }
+  useEffect(() => {
+    detectRoute()
   },[location])  
   return (
-      <StoreProvider>
         <Switch>
-        <Route path="/content">
-          <Content/>
-        </Route>
-        <Route path="/">
-          <Home/>
-        </Route>
+          <Route path="/content">
+            <Content/>
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
         </Switch>
-      </StoreProvider>
   )
 }
 
