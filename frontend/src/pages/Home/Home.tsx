45b73e43352fe3fbf8e6f0cdc0ef8bad12e079c1
tuzunar <176001027@stu.adu.edu.tr>
2020-11-16 19:40:45 +0300

    EDIT: localize support  for string

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -3,14 +3,16 @@ import { Layout } from '../../components'
 import style from './Home.module.scss'
 import cn from'classnames'
 import xbox from '../../image/xbox.png'
+import i18next from 'i18next'
 const Home = () => {
     return (
         <Layout>
-            <div className={style.container}>
+            <div className={style.mainWrapper}>
+                <div className={style.blur}/>
                 <div className={style.search}>
-                    <input type="text" name="" className={cn(style.searchBar, style.bold)} placeholder="Search GameWiki"/>
-                    <p className={cn(style.secondaryText, style.bold, style.font24, style.SearchText)}>
-                        Type what you are looking for...
+                    <input type="text" name="" className={cn(style.searchBar, style.bold, style.font36)} placeholder={i18next.t("SearchInput")}/>
+                    <p className={cn(style.secondaryText, style.bold, style.font22, style.SearchText)}>
+                        {i18next.t('secondaryText')}
                     </p>
                 </div>
                 <div className={style.image}>
