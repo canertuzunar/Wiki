cbb245b0f654eca6b2871161822910512b61bdd2
tuzunar <176001027@stu.adu.edu.tr>
2020-12-07 20:46:01 +0300

    EDIT: Graphql server connection with Apollo

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,22 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {BrowserRouter as Router} from'react-router-dom'
-import { StoreProvider } from './util/store/store';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'normalize.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { StoreProvider } from './util/store/store'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <StoreProvider>
-      <Router>
-        <App />
-      </Router>
-    </StoreProvider>
+    <ApolloProvider client={client}>
+      <StoreProvider>
+        <Router>
+          <App />
+        </Router>
+      </StoreProvider>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
