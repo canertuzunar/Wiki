938d9ed00a9646a1f09059d336b905659bfb731c
tuzunar <176001027@stu.adu.edu.tr>
2020-12-07 20:41:18 +0300

    EDIT: content page id param

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,35 @@
-import i18next from 'i18next';
-import React, { useEffect } from 'react';
-import { Switch, Route, useLocation} from'react-router-dom'
-import { Content, Home } from './pages';
+import i18next from 'i18next'
+import React, { useEffect } from 'react'
+import { Switch, Route, useLocation } from 'react-router-dom'
+import { Content, Home } from './pages'
 import './util/master.scss'
-import {useStore} from'./util/store/store'
-import resources from'./util/localize/resources'
+import { useStore } from './util/store/store'
+import resources from './util/localize/resources'
 
 i18next.init({
   resources,
-  lng: "en"
+  lng: 'en'
 })
 
 const App = () => {
   const location = useLocation()
-  const {state, dispatch} = useStore()
+  const { dispatch } = useStore()
   const detectRoute = () => {
-    dispatch({type:"setShadow", value:false})
-    console.log(location, state)
+    dispatch({ type: 'setShadow', value: false })
   }
   useEffect(() => {
     detectRoute()
-  },[location])  
+  }, [location])
   return (
-        <Switch>
-          <Route path="/content">
-            <Content/>
-          </Route>
-          <Route path="/">
-            <Home/>
-          </Route>
-        </Switch>
+    <Switch>
+      <Route path="/content/:id">
+        <Content />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
   )
 }
 
 export default App
-
