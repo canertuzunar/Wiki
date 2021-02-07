21e46da4fdf3652100b2e639ead1c3ab18538738
canertuzunar <176001027@stu.adu.edu.tr>
2021-02-07 23:46:42 +0300

    DELETE: unuse interfaces, imports and logs

--- frontend/src/components/Search/Search.tsx
+++ frontend/src/components/Search/Search.tsx
@@ -2,22 +2,13 @@ import React, { Fragment, useState } from'react'
 import cnBind from'classnames/bind'
 import styles from './Search.module.scss'
 import i18next from 'i18next'
-import { useQuery } from 'react-apollo'
-import gameGraphql from 'src/graphql/query/game.graphql'
 import { useHistory } from 'react-router-dom'
-interface GameInterface {
-  game: any
-}
-interface GameInterfaceVars {
-  name: string
-}
+
 const Search = () => {
     let history = useHistory()
     let cx = cnBind.bind(styles)
     const [focus, setFocus] = useState(false)
     const [searchParam, setSearchParam]  = useState('')
-    const {loading, error, data } = useQuery<GameInterface, GameInterfaceVars>(gameGraphql, {variables:{name: searchParam}})
-    console.log(loading, error, data)
     const handleSearch = ( searchParam: string ) => {
         history.push(`content/${searchParam.trim()}`)
     }
@@ -35,7 +26,6 @@ const Search = () => {
             onKeyDown = {e => {
                 if(e.key === "Enter"){
                     handleSearch(searchParam)
-                    console.log(e.key)
                 } 
             }}
             onFocus= {e => {
