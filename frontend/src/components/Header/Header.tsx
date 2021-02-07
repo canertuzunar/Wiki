fe4fa4986a5bc30c4513d8e34d4fa88575b32e29
canertuzunar <176001027@stu.adu.edu.tr>
2021-02-07 23:36:57 +0300

    ADD: hamburger menu icon

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -4,13 +4,16 @@ import styles from './Header.module.scss'
 import cn from 'classnames'
 import cnBind from 'classnames/bind'
 import { useStore } from '../../util/store/store'
+// TODO add sign in button 
 const Header = () => {
   const { state } = useStore()
   let cx = cnBind.bind(styles)
   return (
     <header className={cx({ headerAttr: state.headerAttr })}>
       <div className={cx({ headerWrapper: true })}>
-        <p className={cn(styles.menuButton)}>Button</p>
+        <label className={cn(styles.menuButton)}>
+          <span className={cn(styles.line)}></span>
+        </label>
         <Logo />
       </div>
     </header>
