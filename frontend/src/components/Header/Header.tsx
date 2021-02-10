cb39fae5eb4a6297770dffd2cc79d88ad96f6ac2
canertuzunar <176001027@stu.adu.edu.tr>
2021-02-10 15:21:22 +0300

    ADD: sign in button, styles and fix some attr

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -4,6 +4,7 @@ import styles from './Header.module.scss'
 import cn from 'classnames'
 import cnBind from 'classnames/bind'
 import { useStore } from '../../util/store/store'
+import { Link } from 'react-router-dom'
 // TODO add sign in button 
 const Header = () => {
   const { state } = useStore()
@@ -15,6 +16,7 @@ const Header = () => {
           <span className={cn(styles.line)}></span>
         </label>
         <Logo />
+        <Link to="/" className={cn(styles.sign)}>sign in</Link>
       </div>
     </header>
   )
