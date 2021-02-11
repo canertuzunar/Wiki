797a7043c9e37cc7df692c93d9256aea8b83261a
canertuzunar <176001027@stu.adu.edu.tr>
2021-02-12 01:58:50 +0300

    EDIT: delete unused classnames and imports

--- frontend/src/components/Logo/Logo.tsx
+++ frontend/src/components/Logo/Logo.tsx
@@ -2,13 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Logo.module.scss'
 import cnBind from 'classnames/bind'
-import { useStore } from '../../util/store/store'
 
 const Logo = () => {
   let cx = cnBind.bind(styles)
-  const { state } = useStore()
   return (
-    <Link to="/content" className={cx({ logo: true, white: state.headerAttr })}>
+    <Link to="/content" className={cx({ logo: true})}>
       <h2>GameWiki</h2>
     </Link>
   )
