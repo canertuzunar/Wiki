5852f2651ec665cee1f93c7908310710e4269238
canertuzunar <176001027@stu.adu.edu.tr>
2021-02-16 12:06:45 +0300

    ADD: query selector function

---
+++ frontend/src/util/querySelector.tsx
@@ -0,0 +1,15 @@
+import { GameQueries } from '../graphql/query/game.graphql'
+import { EngineQueries } from '../graphql/query/engine.grapqhl'
+
+const querySelector = (route: Object) => {
+    switch(route) {
+        case 'games':
+            return GameQueries.games
+        case 'engines':
+            return EngineQueries.engines 
+        default: 
+            throw Error('unknown input');
+    }
+}
+
+export default querySelector
