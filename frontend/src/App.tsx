c83f1e6b6e72fad818dbbafcc9330c0df6fd77ff
canertuzunar <176001027@stu.adu.edu.tr>
2021-02-16 12:00:27 +0300

    ADD: sub pages route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import i18next from 'i18next'
 import React, { useEffect } from 'react'
 import { Switch, Route, useLocation } from 'react-router-dom'
-import { Content, Home } from './pages'
+import { Content, Home, List } from './pages'
 import './util/master.scss'
 import { useStore } from './util/store/store'
 import resources from './util/localize/resources'
@@ -16,6 +16,7 @@ const App = () => {
   const { dispatch } = useStore()
   const detectRoute = () => {
     dispatch({ type: 'setShadow', value: false })
+    dispatch({ type: 'setMenu', value: false })
   }
   useEffect(() => {
     detectRoute()
@@ -24,6 +25,9 @@ const App = () => {
     <Switch>
       <Route path="/content/:id">
         <Content />
+      </Route>
+      <Route path="/:listType">
+        <List />
       </Route>
       <Route path="/">
         <Home />
