145c6e6445b4c5e4ee3eaa63082b646a099f7fdb
canertuzunar <176001027@stu.adu.edu.tr>
2021-02-28 16:32:52 +0300

    ADD: List Page with Skeleton View

---
+++ frontend/src/components/LoadingElement/LoadingList.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+import { SkeletonCard } from '../index'
+
+const SkeletonContent = () => {
+    const elementStack = []
+    for(let i = 0; i <= 12; i++) {
+        elementStack.push(<SkeletonCard/>)
+    }
+    return(
+        <StyledContent>
+            {elementStack}    
+        </StyledContent>
+    )
+}
+//
+const StyledContent = styled.div`
+    flex-basis: 100%;
+    margin: 0 40px;
+`
+
+export default SkeletonContent
