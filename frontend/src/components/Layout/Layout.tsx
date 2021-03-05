0e6d6569aaba476f7ca80b9aac94d5c9726ca1b9
canertuzunar <176001027@stu.adu.edu.tr>
2021-03-05 15:10:17 +0300

    Update

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,5 +1,5 @@
-import React, { Fragment } from 'react'
-import { useStore } from '../../util/store/store'
+import React, { Fragment, useRef } from 'react'
+import { useStore } from 'src/util/store/store'
 import { Header, Head } from '../index'
 import styles from './Layout.module.scss'
 
@@ -8,21 +8,21 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const LayoutRef = useRef<HTMLDivElement>(null) 
   const { dispatch } = useStore()
-  const scrollDetector = (scrollY: number) => {
-    scrollY > 30
+  const scrollDetector = () => {
+    const element = LayoutRef.current
+    element!.scrollTop > 30
       ? dispatch({ type: 'setShadow', value: true })
       : dispatch({ type: 'setShadow', value: false })
   }
+
   return (
     <Fragment>
       <div
+        ref={LayoutRef}
         className={styles.container}
-        onScroll={(e) => {
-          e.persist()
-          const element = e.target as HTMLBodyElement
-          scrollDetector(element.scrollTop)
-        }}
+        onScroll={scrollDetector}
       >
         <Head />
         <Header />
