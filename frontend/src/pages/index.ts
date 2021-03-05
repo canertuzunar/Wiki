99a313961f0679603158d24b172488e8ff870c5c
canertuzunar <176001027@stu.adu.edu.tr>
2021-03-05 15:10:35 +0300

    easy import

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -2,4 +2,5 @@ import Home from './Home/Home'
 import Content from './Content/Content'
 import List from './List/List'
 import ErrorPage from './ErrorPage/ErrorPage'
-export { Home, Content, List, ErrorPage }
+import LoginPage from './Login/LoginPage'
+export { Home, Content, List, ErrorPage, LoginPage }
