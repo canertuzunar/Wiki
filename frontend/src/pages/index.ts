27c5699f83a71af16a4ef3248c17b2e5db47e96a
canertuzunar <tuzunarcaner@gmail.com>
2023-01-07 17:59:07 +0300

    deleted strapi

--- frontend/src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-import Home from './Home/Home'
-import Content from './Content/Content'
-import List from './List/List'
-import ErrorPage from './ErrorPage/ErrorPage'
-export { Home, Content, List, ErrorPage, }
--- frontend/src/util/splitAndConvert.ts
+++
@@ -1,8 +0,0 @@
-function splitAndConverts(data:any) {
-    const [value, type] = data.match(/(\d+)|(\D+$)/g)!
-    const INTVALUE = parseFloat(value)
-
-    return [INTVALUE, type]
-}
-
-export default splitAndConverts
